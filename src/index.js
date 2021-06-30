df48b7a9628539a3777473ed754ad1990c4a7be9
sebastiansalvat <sebastianestebansalvat@gmail.com>
2021-06-30 11:45:24 -0300

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
+
+
+
 //Server
-app.listen(3000,() => console.log("Server on http://localhost:3000"));
+app.listen(process.env.PORT || 3000,() => console.log("Server on http://localhost:3000"));
 
 //Public Access
 app.use(express.static(path.resolve(__dirname, "../public")));
